a220e6dff57d67b01e95b3ca6c47015a5033971d
Flavio <82716912+flavio641@users.noreply.github.com>
2023-01-29 20:14:48 -0300

    atualizando

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.secret_key = 'flavio'
 
 mail_settings = {
     "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT":495,
+    "MAIL_PORT": 495,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
     "MAIL_USERNAME": email,
